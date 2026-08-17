2b90c16973f111ba7071983269e77dc83f64d7ae
Karthiga Mohan <karthigamohann@gmail.com>
2026-08-17 19:27:17 +0530

    Create 485-MaxConsecutiveOnes

---
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.c
@@ -0,0 +1,16 @@
+// Last updated: 8/17/2026, 7:27:17 PM
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+    int maxCount = 0, count = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == 1) {
+            count++;
+            if (count > maxCount)
+                maxCount = count;
+        } else {
+            count = 0;
+        }
+    }
+
+    return maxCount;
+}
