86a75b77ed58d21ddbe53572d36940da571258e9
Karthiga Mohan <karthigamohann@gmail.com>
2026-08-17 19:30:58 +0530

    Create 6-ZigzagConversion

---
+++ 6-ZigzagConversion/6-ZigzagConversion.py
@@ -0,0 +1,20 @@
+# Last updated: 8/17/2026, 7:30:59 PM
+class Solution:
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        cur_row = 0
+        going_down = False
+
+        for char in s:
+            rows[cur_row] += char
+            
+            # Change direction at top or bottom
+            if cur_row == 0 or cur_row == numRows - 1:
+                going_down = not going_down
+
+            cur_row += 1 if going_down else -1
+
+        return "".join(rows)
