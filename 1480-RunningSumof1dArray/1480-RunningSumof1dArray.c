7cbadc14c78dee903bfd10158f4e81a58cfef14b
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-08 11:59:48 +0530

    File updated at 9/8/2026, 11:59:49 AM

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.c
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.c
@@ -1,13 +1,15 @@
-// Last updated: 9/8/2026, 11:28:57 AM
-1/**
-2 * Note: The returned array must be malloced, assume caller calls free().
-3 */
-4int* runningSum(int* nums, int numsSize, int* returnSize) {
-5    *returnSize = numsSize;
-6    int *prefix = (int*)malloc(numsSize*sizeof(int));
-7    prefix[0] = nums[0];
-8    for(int i=1; i < numsSize ; i++){
-9        prefix[i] = prefix[i-1]+ nums[i];
-10    }
-11    return prefix;
-12}
+// Last updated: 9/8/2026, 11:59:49 AM
+1int pivotIndex(int* nums, int numsSize) {
+2    int * pf = (int*)malloc((numsSize+1) * sizeof(int));
+3    pf[0] =0;
+4    for(int i = 1; i <= numsSize; i++){
+5        pf[i] = pf[i-1]+nums[i-1];
+6    }
+7
+8    for(int i =1; i <= numsSize ; i++){
+9        if(pf[i-1] == pf[numsSize]- pf[i]){
+10            return i-1;
+11        }
+12    }
+13    return -1;
+14}
