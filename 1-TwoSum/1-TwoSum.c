6ccc0b2e89ce0843a293210c52bf9d759cd745e7
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-08 09:03:29 +0530

    File updated at 9/8/2026, 9:03:28 AM

--- 1-TwoSum/1-TwoSum.c
+++ 1-TwoSum/1-TwoSum.c
@@ -1,14 +1,14 @@
-// Last updated: 8/17/2026, 7:31:35 PM
-int* twoSum(int* nums, int  N, int target, int* returnSize){
-    int *arr = malloc(2*sizeof(int));
-    *returnSize = 2;
-    for(int i=0; i < N-1; i++){
-        for(int j=i+1; j < N; j++){
-            if(nums[i] + nums[j] == target){
-                arr[0] = i; arr[1] = j;
-                return arr;
-            }
-        }
-    }
-    return arr;
-}
+// Last updated: 9/8/2026, 9:03:28 AM
+1int* twoSum(int* nums, int  N, int target, int* returnSize){
+2    int *arr = malloc(2*sizeof(int));
+3    *returnSize = 2;
+4    for(int i=0; i < N-1; i++){
+5        for(int j=i+1; j < N; j++){
+6            if(nums[i] + nums[j] == target){
+7                arr[0] = i; arr[1] = j;
+8                return arr;
+9            }
+10        }
+11    }
+12    return arr;
+13}
