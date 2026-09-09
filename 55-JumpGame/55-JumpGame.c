6b7cfcebfc06d392b010c989e8f5bfbf6f0ef09d
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-09 10:02:14 +0530

    File updated at 9/9/2026, 10:02:15 AM

--- 55-JumpGame/55-JumpGame.c
+++ 55-JumpGame/55-JumpGame.c
@@ -1,15 +1,21 @@
-// Last updated: 9/9/2026, 9:21:58 AM
-1int addDigits(int num) {
-2    while(num >=10){
-3        int sum=0;
+// Last updated: 9/9/2026, 10:02:15 AM
+1int findMaxConsecutiveOnes(int* nums, int numsSize) {
+2    int count = 0;
+3    int max_count = 0;
 4
-5
-6    while(num !=0){
-7        sum = sum + (num%10);
-8        num = num/10;
-9    }
-10    num = sum;
-11    }
-12
-13    return num;
-14}
+5    for(int i = 0; i < numsSize; i++) {
+6
+7        if(nums[i] == 1) {
+8            count++;
+9        }
+10        else {
+11            count = 0;
+12        }
+13
+14        if(count > max_count) {
+15            max_count = count;
+16        }
+17    }
+18
+19    return max_count;
+20}
