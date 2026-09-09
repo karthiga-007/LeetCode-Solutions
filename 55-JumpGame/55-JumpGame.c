cd5aae2938bd3df10f5b836fb194ca02fae222e8
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-09 09:21:57 +0530

    File updated at 9/9/2026, 9:21:58 AM

--- 55-JumpGame/55-JumpGame.c
+++ 55-JumpGame/55-JumpGame.c
@@ -1,18 +1,15 @@
-// Last updated: 9/9/2026, 8:58:56 AM
-1bool canJump(int* nums, int numsSize) {
-2
-3    int farthest = 0;
+// Last updated: 9/9/2026, 9:21:58 AM
+1int addDigits(int num) {
+2    while(num >=10){
+3        int sum=0;
 4
-5    for (int i = 0; i < numsSize; i++) {
-6
-7        if (i > farthest) {
-8            return false;
-9        }
-10
-11        if (i + nums[i] > farthest) {
-12            farthest = i + nums[i];
-13        }
-14    }
-15
-16    return true;
-17}
+5
+6    while(num !=0){
+7        sum = sum + (num%10);
+8        num = num/10;
+9    }
+10    num = sum;
+11    }
+12
+13    return num;
+14}
