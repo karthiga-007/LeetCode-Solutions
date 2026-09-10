e6bb173dc5294cdaf61a2220e2b48a304fe00d4d
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-10 13:48:29 +0530

    File updated at 9/10/2026, 1:48:29 PM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.c
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.c
@@ -1,26 +1,21 @@
-// Last updated: 9/10/2026, 11:20:48 AM
-1int numSubarrayProductLessThanK(int* nums, int numsSize, int k) {
-2    long long product;
-3    int count = 0;
-4
-5    for (int i = 0; i < numsSize; i++) {
-6
-7        product = 1;
-8
-9        for (int j = i; j < numsSize; j++) {
-10
-11            // Prevent overflow before multiplying
-12            if (product >= k ) {
-13                break;
-14            }
-15
-16            product = product * nums[j];
-17
-18            if (product < k) {
-19                count++;
-20            }
-21        }
-22    }
-23
-24    return count;
-25}
+// Last updated: 9/10/2026, 1:48:29 PM
+1bool isAnagram(const char *s, const char *t) {
+2    if (strlen(s) != strlen(t)) {
+3        return false;
+4    }
+5
+6    int count[26] = {0};
+7
+8    for (int i = 0; i < strlen(s); i++) {
+9        count[s[i] - 'a']++;
+10        count[t[i] - 'a']--;
+11    }
+12
+13    for (int i = 0; i < 26; i++) {
+14        if (count[i] != 0) {
+15            return false;
+16        }
+17    }
+18
+19    return true;
+20}
