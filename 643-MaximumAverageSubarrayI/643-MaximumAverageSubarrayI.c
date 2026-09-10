4d74e4d4807f19b66d0ad03c521a6a75064b79fa
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-10 09:19:18 +0530

    File updated at 9/10/2026, 9:19:18 AM

--- 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.c
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.c
@@ -1,22 +1,19 @@
-// Last updated: 9/10/2026, 9:04:14 AM
-1double findMaxAverage(int* nums, int numsSize, int k) {
-2     int sum = 0;
-3
-4     for(int i = 0 ; i < k; i++){
-5        sum = sum + nums[i];
-6     }
-7
-8     int max_sum = sum;
-9
-10     for(int i = 0; i < numsSize - k; i++){
-11        sum = sum - nums[i] + nums[i+k];
-12     
-13
-14     if(sum > max_sum){
-15        max_sum = sum;
-16     }
-17     }
-18
-19
-20return(double)max_sum/k;
-21}
+// Last updated: 9/10/2026, 9:19:18 AM
+1bool isPalindrome(int x) {
+2
+3    int og = x;
+4    long long reverse = 0;
+5    if(x < 0){
+6        return false;
+7    }
+8
+9while(x !=0){
+10      int num = x%10;
+11      reverse = reverse*10+num;
+12      x=x/10;
+13    }
+14    if(reverse == og){
+15        return true;
+16    }
+17    return 0;
+18}
