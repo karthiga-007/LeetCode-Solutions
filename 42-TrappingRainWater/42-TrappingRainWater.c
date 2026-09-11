367313520f156b5a5b63cf84740d913bb539a112
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-11 11:20:32 +0530

    File updated at 9/11/2026, 11:20:31 AM

--- 42-TrappingRainWater/42-TrappingRainWater.c
+++ 42-TrappingRainWater/42-TrappingRainWater.c
@@ -1,27 +1,14 @@
-// Last updated: 9/11/2026, 10:06:42 AM
-1int trap(int* height, int heightSize) {
-2    int total = 0;
-3    int l = 0, r = heightSize - 1; 
-4    int lmax = 0, rmax = height[r];
-5    
-6    while (l < r) {
-7        if (height[l] <= height[r]) {
-8           
-9            if (height[l] < lmax) {
-10                total += lmax - height[l]; 
-11            } else {
-12                lmax = height[l];
-13            }
-14            l++; 
-15        } else {
-16           
-17            if (height[r] < rmax) {
-18                total += rmax - height[r]; 
-19            } else {
-20                rmax = height[r]; 
-21            }
-22            r--;
-23        }
-24    }
-25    return total; 
-26} 
+// Last updated: 9/11/2026, 11:20:31 AM
+1void reverseString(char* s, int sSize) {
+2    char temp;
+3
+4    for (int i = 0; i < sSize / 2; i++) {
+5        for (int j = sSize - 1 - i; j < sSize; j++) {
+6            temp = s[i];
+7            s[i] = s[j];
+8            s[j] = temp;
+9             break;
+10
+11        }
+12    }
+13}
