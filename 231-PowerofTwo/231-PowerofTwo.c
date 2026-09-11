af2b876f01bdb1b6d41e75f71edbf8ef2c831204
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-11 09:16:15 +0530

    Create 231-PowerofTwo

---
+++ 231-PowerofTwo/231-PowerofTwo.c
@@ -0,0 +1,10 @@
+// Last updated: 9/11/2026, 9:16:14 AM
+1bool isPowerOfTwo(int n){
+2    if(n == 1)
+3        return true;
+4    else if(n == 0)
+5        return false;
+6    else if(n % 2 != 0)
+7        return false;
+8    return isPowerOfTwo(n/2);
+9}
