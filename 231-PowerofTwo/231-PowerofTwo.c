979f9cb2b41533cde25ab847d85bd8d65d6a4da2
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-11 09:59:33 +0530

    File updated at 9/11/2026, 9:59:33 AM

--- 231-PowerofTwo/231-PowerofTwo.c
+++ 231-PowerofTwo/231-PowerofTwo.c
@@ -1,10 +1,10 @@
-// Last updated: 9/11/2026, 9:41:31 AM
-1bool isPowerOfTwo(int n){
-2    if(n == 1)
-3        return true;
-4    else if(n == 0)
-5        return false;
-6    else if(n % 2 != 0)
-7        return false;
-8    return isPowerOfTwo(n/2);
+// Last updated: 9/11/2026, 9:59:33 AM
+1int singleNumber(int* nums, int numsSize) {
+2    int result = 0;
+3
+4    for(int i=0; i < numsSize ; i++){
+5        result = result ^ nums[i];
+6    }
+7
+8    return result;
 9}
