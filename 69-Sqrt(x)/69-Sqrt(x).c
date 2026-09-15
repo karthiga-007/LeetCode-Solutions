232f4f0d4d1846468d88ef0c4b5340856648d535
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-15 20:14:44 +0530

    File updated at 9/15/2026, 8:14:44 PM

--- 69-Sqrt(x)/69-Sqrt(x).c
+++ 69-Sqrt(x)/69-Sqrt(x).c
@@ -1,19 +1,13 @@
-// Last updated: 9/15/2026, 8:14:22 PM
+// Last updated: 9/15/2026, 8:14:44 PM
 1int mySqrt(int x) {
-2    int left = 0;
-3    int right = x >> 1;    //same as right = x / 2
+2    if (x == 0)
+3        return 0;
 4
-5    if(x < 2)
-6        return x;
-7    
-8    while(left <= right){
-9        long mid = (left + right) >> 1;        //same as mid = (left + right) / 2;
-10        if(mid * mid == x)
-11            return mid;
-12        else if(mid * mid < x)
-13            left = mid + 1;
-14        else
-15            right = mid - 1; 
-16    }
-17    return left - 1;
-18}
+5    int i = 1;
+6
+7    while (i <= x / i) {
+8        i++;
+9    }
+10
+11    return i - 1;
+12}
