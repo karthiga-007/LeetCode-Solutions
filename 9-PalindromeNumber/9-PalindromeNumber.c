872b5610cb9eeb9025296ac4d20440428c1c8857
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-15 11:04:26 +0530

    File updated at 9/15/2026, 11:04:25 AM

--- 9-PalindromeNumber/9-PalindromeNumber.c
+++ 9-PalindromeNumber/9-PalindromeNumber.c
@@ -1,20 +1,10 @@
-// Last updated: 9/15/2026, 10:50:17 AM
-1char **fizzBuzz(int n, int* returnSize) {
-2    *returnSize = n;
-3    char **answer = malloc(n * sizeof(char *));
-4    
-5    for (int i = 1; i <= n; i++) {
-6        answer[i - 1] = malloc(19);
-7        if (i % 15 == 0) {
-8            strcpy(answer[i - 1], "FizzBuzz");
-9        } else if (i % 3 == 0) {
-10            strcpy(answer[i - 1], "Fizz");
-11        } else if (i % 5 == 0) {
-12            strcpy(answer[i - 1], "Buzz");
-13        } else {
-14            sprintf(answer[i - 1], "%d", i);
-15        }
-16    }
-17    
-18    return answer;
-19}
+// Last updated: 9/15/2026, 11:04:25 AM
+1bool isPowerOfTwo(int n){
+2    if(n == 1)
+3        return true;
+4    else if(n == 0)
+5        return false;
+6    else if(n % 2 != 0)
+7        return false;
+8    return isPowerOfTwo(n/2);
+9}
