46ccf2554368dcbcc0102fb0cbc48d80125a1bb8
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-15 09:54:56 +0530

    File updated at 9/15/2026, 9:54:55 AM

--- 258-AddDigits/258-AddDigits.c
+++ 258-AddDigits/258-AddDigits.c
@@ -1,15 +1,17 @@
-// Last updated: 9/9/2026, 9:54:46 AM
-int addDigits(int num) {
-    while(num >=10){
-        int sum=0;
-
-
-    while(num !=0){
-        sum = sum + (num%10);
-        num = num/10;
-    }
-    num = sum;
-    }
-
-    return num;
-}
+// Last updated: 9/15/2026, 9:54:55 AM
+1int addDigits(int num) {
+2    while(num>=10){
+3
+4        int sum = 0;
+5
+6        while(num !=0){
+7            sum = sum + num % 10;
+8            num = num/ 10;
+9        }
+10      
+11      num = sum;
+12
+13    }
+14
+15    return num;
+16}
