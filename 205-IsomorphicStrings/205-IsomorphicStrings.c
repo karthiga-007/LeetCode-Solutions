12268cee0e21065bb88abda75688995c96c3f950
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-15 13:57:52 +0530

    File updated at 9/15/2026, 1:57:52 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.c
+++ 205-IsomorphicStrings/205-IsomorphicStrings.c
@@ -1,11 +1,14 @@
-// Last updated: 9/15/2026, 1:29:01 PM
-1int firstUniqChar(char* s) {
-2    int fr[256]={0};
-3    for(int i=0; i < strlen(s); i++)
-4    fr[s[i]]++;
-5    for(int i=0; i < strlen(s); i++){
-6        if(fr[s[i]]==1)
-7        return i;
-8    }
-9    return -1;
-10}
+// Last updated: 9/15/2026, 1:57:52 PM
+1bool canConstruct(char* r, char* m) {
+2    int fr1[256] = {0}, fr2[256]={0};
+3    for(int i=0; i < strlen(r); i++)
+4    fr1[r[i]]++;
+5    for(int i=0; i < strlen(m);i++)
+6    fr2[m[i]]++;
+7
+8    for(int i=0; i < strlen(r);i++){
+9        if(fr1[r[i]]!= fr2[r[i]]&&fr1[r[i]]>fr2[r[i]])
+10        return false;
+11    }
+12    return true;
+13}
