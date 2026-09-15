12e7b35e8c874283f5b4d6539b2a7b612af0ff57
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-15 11:07:28 +0530

    File updated at 9/15/2026, 11:07:28 AM

--- 9-PalindromeNumber/9-PalindromeNumber.c
+++ 9-PalindromeNumber/9-PalindromeNumber.c
@@ -1,10 +1,16 @@
-// Last updated: 9/15/2026, 11:04:25 AM
-1bool isPowerOfTwo(int n){
-2    if(n == 1)
+// Last updated: 9/15/2026, 11:07:28 AM
+1bool isPowerOfTwo(int n) {
+2    if(n == 1){
 3        return true;
-4    else if(n == 0)
-5        return false;
-6    else if(n % 2 != 0)
+4    }
+5
+6    else if(n ==0){
 7        return false;
-8    return isPowerOfTwo(n/2);
-9}
+8    }
+9
+10    else if(n%2 != 0){
+11        return false;
+12    }
+13
+14    return isPowerOfTwo(n/2);
+15}
