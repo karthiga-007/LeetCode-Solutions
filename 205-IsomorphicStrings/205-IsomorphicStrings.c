912058175c4e2e596b4225b3cb7225cf153f5d97
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-15 13:29:02 +0530

    File updated at 9/15/2026, 1:29:01 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.c
+++ 205-IsomorphicStrings/205-IsomorphicStrings.c
@@ -1,26 +1,11 @@
-// Last updated: 9/15/2026, 11:52:42 AM
-1char* frequencySort(char* s) {
-2    char*res=malloc(strlen(s)+1*sizeof(char));
-3    int fr[256]={0};
-4  
-5  for(int i=0;i<strlen(s);i++)
-6  fr[s[i]]++;
-7  
-8   int index=0;
-9    for(int i=strlen(s);i>=1;i--)
-10    {
-11        for(int j=0;j<256;j++)
-12        {
-13            if(fr[j]==i)
-14            {
-15                for(int k=0;k<i;k++)
-16                {
-17                    res[index++]=(char)j;
-18                }
-19            }
-20        }
-21    }
-22    res[index]='\0';
-23    return res;
-24
-25}
+// Last updated: 9/15/2026, 1:29:01 PM
+1int firstUniqChar(char* s) {
+2    int fr[256]={0};
+3    for(int i=0; i < strlen(s); i++)
+4    fr[s[i]]++;
+5    for(int i=0; i < strlen(s); i++){
+6        if(fr[s[i]]==1)
+7        return i;
+8    }
+9    return -1;
+10}
