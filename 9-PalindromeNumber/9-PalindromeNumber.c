b24082715029bb5700b3e6e2ac2a0dc129ee71a7
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-15 10:50:18 +0530

    File updated at 9/15/2026, 10:50:17 AM

--- 9-PalindromeNumber/9-PalindromeNumber.c
+++ 9-PalindromeNumber/9-PalindromeNumber.c
@@ -1,21 +1,20 @@
-// Last updated: 9/15/2026, 10:12:20 AM
-1bool isPalindrome(int x) {
-2    if(x < 0){
-3        return false;
-4    }
-5
-6    int og = x;
-7    long long rev = 0;
-8
-9    while(x != 0){
-10        int i = x % 10;
-11        rev = rev * 10 + i;
-12        x = x / 10;
-13    }
-14
-15    if(rev == og){
-16        return true;
-17    }
-18
-19    return false;
-20}
+// Last updated: 9/15/2026, 10:50:17 AM
+1char **fizzBuzz(int n, int* returnSize) {
+2    *returnSize = n;
+3    char **answer = malloc(n * sizeof(char *));
+4    
+5    for (int i = 1; i <= n; i++) {
+6        answer[i - 1] = malloc(19);
+7        if (i % 15 == 0) {
+8            strcpy(answer[i - 1], "FizzBuzz");
+9        } else if (i % 3 == 0) {
+10            strcpy(answer[i - 1], "Fizz");
+11        } else if (i % 5 == 0) {
+12            strcpy(answer[i - 1], "Buzz");
+13        } else {
+14            sprintf(answer[i - 1], "%d", i);
+15        }
+16    }
+17    
+18    return answer;
+19}
