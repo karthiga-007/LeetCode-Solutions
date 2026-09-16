b06ac53ada018801c9b57058e42c917e57053455
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-16 14:23:15 +0530

    File updated at 9/16/2026, 2:23:15 PM

--- 69-Sqrt(x)/69-Sqrt(x).c
+++ 69-Sqrt(x)/69-Sqrt(x).c
@@ -1,39 +1,43 @@
-// Last updated: 9/16/2026, 10:01:21 AM
-1bool isPalindrome(char s[], int left, int right)
-2{
-3    while (left < right)
-4    {
-5        if (s[left] != s[right])
-6            return false;
+// Last updated: 9/16/2026, 2:23:15 PM
+1int compress(char* chars, int charsSize) {
+2    int index = 0;
+3
+4    for (int i = 0; i < charsSize;) {
+5        int c = 0;
+6        char ch = chars[i];
 7
-8        left++;
-9        right--;
-10    }
-11
-12    return true;
-13}
-14
-15bool validPalindrome(char s[])
-16{
-17    int left = 0;
-18    int right = strlen(s) - 1;
+8        // Count how many times ch occurs continuously
+9        while (i < charsSize && chars[i] == ch) {
+10            c++;
+11            i++;
+12        }
+13
+14        chars[index] = ch;
+15        index++;
+16        // Store count if
+17        if (c > 1) {
+18            int start = index;
 19
-20    while (left < right)
-21    {
-22        if (s[left] != s[right])
-23        {
-24            if (isPalindrome(s, left + 1, right))
-25                return true;
+20            // Write the count digits
+21            while (c > 0) {
+22                chars[index] = (c % 10) + '0';
+23                index++;
+24                c = c / 10;
+25            }
 26
-27            if (isPalindrome(s, left, right - 1))
-28                return true;
-29
-30            return false;
-31        }
-32
-33        left++;
-34        right--;
-35    }
-36
-37    return true;
-38}
+27            // Reverse the digits
+28            int left = start;
+29            int right = index - 1;
+30
+31            while (left < right) {
+32                char temp = chars[left];
+33                chars[left] = chars[right];
+34                chars[right] = temp;
+35                left++;
+36                right--;
+37            }
+38        }
+39    }
+40
+41    return index;
+42}
