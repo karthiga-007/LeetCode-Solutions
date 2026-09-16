7b739bd6be50abd1c30cafa87e037937cf9c88e7
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-16 08:46:52 +0530

    File updated at 9/16/2026, 8:46:52 AM

--- 69-Sqrt(x)/69-Sqrt(x).c
+++ 69-Sqrt(x)/69-Sqrt(x).c
@@ -1,13 +1,25 @@
-// Last updated: 9/15/2026, 8:18:22 PM
-1int mySqrt(int x) {
-2    if (x == 0)
-3        return 0;
-4
-5    int i = 1;
+// Last updated: 9/16/2026, 8:46:52 AM
+1int longestPalindrome(char* s) {
+2    int fr[256] = {0};
+3
+4    for(int i = 0; i < strlen(s); i++)
+5        fr[s[i]]++;
 6
-7    while (i <= x / i) {
-8        i++;
-9    }
-10
-11    return i - 1;
-12}
+7    int len = 0;
+8    int odd = 0;
+9
+10    for(int i = 0; i < 256; i++) {
+11        if(fr[i] % 2 == 0) {
+12            len += fr[i];
+13        }
+14        else {
+15            len += fr[i] - 1;
+16            odd = 1;
+17        }
+18    }
+19
+20    if(odd == 1)
+21        len++;
+22
+23    return len;
+24}
