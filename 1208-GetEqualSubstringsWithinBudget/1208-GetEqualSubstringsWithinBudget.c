e116f6f0353f6595b47212572402af6aa14db9c0
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-17 11:30:16 +0530

    File updated at 9/17/2026, 11:30:16 AM

--- 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.c
+++ 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.c
@@ -1,18 +1,43 @@
-// Last updated: 9/17/2026, 10:36:55 AM
-1int numberOfSubstrings(char* s) {
-2    int j = 0;
-3    int count[3] = {0};
-4    int res = 0;
-5
-6    for (int i = 0; s[i] != '\0'; i++) {
-7        count[s[i] - 'a']++;
-8
-9        while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
-10            res += strlen(s) - i;
-11            count[s[j] - 'a']--;
-12            j++;
-13        }
-14    }
-15
-16    return res;
-17}
+// Last updated: 9/17/2026, 11:30:16 AM
+1int balancedString(char* s) {
+2    int fr[256] = {0};
+3
+4    for (int i = 0; i < strlen(s); i++) {
+5        fr[s[i]]++;
+6    }
+7
+8    int req = strlen(s) / 4;
+9
+10    int extraQ = fr['Q'] - req;
+11    int extraW = fr['W'] - req;
+12    int extraE = fr['E'] - req;
+13    int extraR = fr['R'] - req;
+14
+15    if (extraQ <= 0 && extraW <= 0 &&
+16        extraE <= 0 && extraR <= 0) {
+17        return 0;
+18    }
+19
+20    int j = 0;
+21    int minlen = strlen(s);
+22
+23    for (int i = 0; i < strlen(s); i++) {
+24
+25        fr[s[i]]--;
+26
+27        while (fr['Q'] <= req &&
+28               fr['W'] <= req &&
+29               fr['E'] <= req &&
+30               fr['R'] <= req) {
+31
+32            if (i - j + 1 < minlen) {
+33                minlen = i - j + 1;
+34            }
+35
+36            fr[s[j]]++;
+37            j++;
+38        }
+39    }
+40
+41    return minlen;
+42}
