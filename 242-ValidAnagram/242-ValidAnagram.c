754ee2a80ae3553d988fe9280237526bde341b67
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-17 13:34:16 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.c
@@ -0,0 +1,21 @@
+// Last updated: 9/17/2026, 1:34:16 PM
+bool isAnagram(const char *s, const char *t) {
+    if (strlen(s) != strlen(t)) {
+        return false;
+    }
+
+    int count[26] = {0};
+
+    for (int i = 0; i < strlen(s); i++) {
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (count[i] != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
