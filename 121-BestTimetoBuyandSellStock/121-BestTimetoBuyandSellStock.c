73036cbcafc5dd3771e1ce91ce634fdc75d77d0d
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-17 10:06:59 +0530

    File updated at 9/17/2026, 10:06:59 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.c
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.c
@@ -1,19 +1,15 @@
-// Last updated: 9/17/2026, 8:51:44 AM
-1int maxProfit(int* prices, int pricesSize) {
-2    int max = prices[0];
-3    int max_profit = 0;
-4
-5    for(int i=0; i < pricesSize;i++){
-6        if(prices[i]<max){
-7            max = prices[i];
-8        }
-9       int profit = prices[i]- max;
-10        if(profit > max_profit){
-11            max_profit = profit;
-12 }
-13
-14 }
-15         return max_profit;
-16    }
-17
-18
+// Last updated: 9/17/2026, 10:06:59 AM
+1void moveZeroes(int* nums, int numsSize) {
+2    int k = 0;
+3    for(int i=0; i < numsSize; i++){
+4        if(nums[i]!=0){
+5            nums[k]=nums[i];
+6            k++;
+7        }
+8    }
+9
+10    for(int j=k; j < numsSize; j++ ){
+11        nums[j]=0;
+12        
+13    }
+14}
