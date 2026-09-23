de928ffd73d5a36b3bedbd11b2fbf4257fba48a0
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-23 11:51:39 +0530

    File updated at 9/23/2026, 11:51:38 AM

--- 179-LargestNumber/179-LargestNumber.c
+++ 179-LargestNumber/179-LargestNumber.c
@@ -1,34 +1,25 @@
-// Last updated: 9/23/2026, 11:20:44 AM
-1#include <stdio.h>
-2#include <stdlib.h>
-3#include <string.h>
+// Last updated: 9/23/2026, 11:51:38 AM
+1int* rearrangeArray(int* nums, int numsSize, int* returnSize) {
+2int *ans = malloc(numsSize * sizeof(int));
+3
 4
-5int compare(const void *a, const void *b) {
-6    char ab[22], ba[22];
+5int pos = 0;
+6int neg = 1;
 7
-8    sprintf(ab, "%d%d", *(int*)a, *(int*)b);
-9    sprintf(ba, "%d%d", *(int*)b, *(int*)a);
-10
-11    return strcmp(ba, ab);
-12}
-13
-14char* largestNumber(int* nums, int numsSize) {
-15    
-16    qsort(nums, numsSize, sizeof(int), compare);
-17
-18    char *ans = malloc(numsSize * 12 + 1);
-19    ans[0] = '\0';
-20
-21    for(int i = 0; i < numsSize; i++) {
-22        char temp[12];
-23
-24        sprintf(temp, "%d", nums[i]);
-25        strcat(ans, temp);
-26    }
-27
-28    if(ans[0] == '0') {
-29        ans[1] = '\0';
-30    }
-31
-32    return ans;
-33}
+8for(int i = 0; i < numsSize; i++) {
+9    if(nums[i] > 0) {
+10        ans[pos] = nums[i];
+11        pos += 2;
+12    }
+13    else {
+14        ans[neg] = nums[i];
+15        neg += 2;
+16    }
+17}
+18
+19*returnSize = numsSize;
+20return ans;
+21
+22
+23}
+24
