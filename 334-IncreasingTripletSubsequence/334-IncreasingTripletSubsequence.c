d60aec3bfd02da32ba2db295a67dfaace32239f0
Karthiga Mohan <karthigamohann@gmail.com>
2026-09-23 10:06:21 +0530

    File updated at 9/23/2026, 10:06:20 AM

--- 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.c
+++ 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.c
@@ -1,13 +1,13 @@
-// Last updated: 9/23/2026, 8:51:11 AM
-1bool increasingTriplet(int* nums, int numsSize) {
-2    int fmin= INT_MAX, smin = INT_MAX;
-3    for(int i=0; i < numsSize;i++){
-4        if(nums[i]<=fmin)
-5        fmin=nums[i];
-6        else if(nums[i]<=smin)
-7        smin=nums[i];
-8        else
-9        return true;
+// Last updated: 9/23/2026, 10:06:20 AM
+1int wiggleMaxLength(int* nums, int numsSize) {
+2    if(numsSize==0)
+3    return 0;
+4    int up=1,dwn=1;
+5    for(int i=1;i<numsSize;i++){
+6        if(nums[i]>nums[i-1])
+7        up=dwn+1;
+8        else if(nums[i]<nums[i-1])
+9        dwn=up+1;
 10    }
-11    return false;
+11    return up>dwn?up:dwn;
 12}
